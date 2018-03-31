a9f60952625152b232d09ed85e6c456eaa84d8af
Kir Kolyshkin <kolyshkin@gmail.com>
2018-03-30 19:15:32 -0700

    selinux_test.go: use t.Skip()
    Instead of enclosing all the test body in an if block,
    use t.Skip(). This avoids an unnecessary indentation,
    and results in more correct output (tests are skipped,
    not passed).
    
    With --ignore-space-change, this patch looks like
    
    -       if GetEnabled() {
    +       if !GetEnabled() {
    +               t.Skip()
    +       }
    ...
    -	}
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- go-selinux/selinux_test.go
+++ go-selinux/selinux_test.go
@@ -8,96 +8,101 @@ import (
 )
 
 func TestSetFileLabel(t *testing.T) {
-	if GetEnabled() {
-		tmp := "selinux_test"
-		con := "system_u:object_r:bin_t:s0"
-		out, _ := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE, 0)
-		out.Close()
-		defer os.Remove(tmp)
-		err := SetFileLabel(tmp, con)
-		if err != nil {
-			t.Log("Setfilecon failed")
-			t.Fatal(err)
-		}
-		filelabel, err := FileLabel(tmp)
-		if err != nil {
-			t.Log("FileLabel failed")
-			t.Fatal(err)
-		}
-		if con != filelabel {
-			t.Fatalf("FileLabel failed, returned %s expected %s", filelabel, con)
-		}
+	if !GetEnabled() {
+		t.Skip()
+	}
+	tmp := "selinux_test"
+	con := "system_u:object_r:bin_t:s0"
+	out, _ := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE, 0)
+	out.Close()
+	defer os.Remove(tmp)
+	err := SetFileLabel(tmp, con)
+	if err != nil {
+		t.Log("Setfilecon failed")
+		t.Fatal(err)
+	}
+	filelabel, err := FileLabel(tmp)
+	if err != nil {
+		t.Log("FileLabel failed")
+		t.Fatal(err)
+	}
+	if con != filelabel {
+		t.Fatalf("FileLabel failed, returned %s expected %s", filelabel, con)
 	}
 }
 
 func TestSELinux(t *testing.T) {
+	if !GetEnabled() {
+		t.Skip()
+	}
+
 	var (
 		err            error
 		plabel, flabel string
 	)
 
-	if GetEnabled() {
-		t.Log("Enabled")
-		plabel, flabel = ContainerLabels()
-		t.Log(plabel)
-		t.Log(flabel)
-		ReleaseLabel(plabel)
-		plabel, flabel = ContainerLabels()
-		t.Log(plabel)
-		t.Log(flabel)
-		ReleaseLabel(plabel)
-		t.Log("Enforcing Mode", EnforceMode())
-		mode := DefaultEnforceMode()
-		t.Log("Default Enforce Mode ", mode)
+	t.Log("Enabled")
+	plabel, flabel = ContainerLabels()
+	t.Log(plabel)
+	t.Log(flabel)
+	ReleaseLabel(plabel)
+	plabel, flabel = ContainerLabels()
+	t.Log(plabel)
+	t.Log(flabel)
+	ReleaseLabel(plabel)
+	t.Log("Enforcing Mode", EnforceMode())
+	mode := DefaultEnforceMode()
+	t.Log("Default Enforce Mode ", mode)
 
-		defer SetEnforceMode(mode)
-		if err := SetEnforceMode(Enforcing); err != nil {
-			t.Fatalf("enforcing selinux failed: %v", err)
-		}
-		if err := SetEnforceMode(Permissive); err != nil {
-			t.Fatalf("setting selinux mode to permissive failed: %v", err)
-		}
-		SetEnforceMode(mode)
-
-		pid := os.Getpid()
-		t.Logf("PID:%d MCS:%s\n", pid, intToMcs(pid, 1023))
-		err = SetFSCreateLabel("unconfined_u:unconfined_r:unconfined_t:s0")
-		if err == nil {
-			t.Log(FSCreateLabel())
-		} else {
-			t.Log("SetFSCreateLabel failed", err)
-			t.Fatal(err)
-		}
-		err = SetFSCreateLabel("")
-		if err == nil {
-			t.Log(FSCreateLabel())
-		} else {
-			t.Log("SetFSCreateLabel failed", err)
-			t.Fatal(err)
-		}
-		t.Log(PidLabel(1))
+	defer SetEnforceMode(mode)
+	if err := SetEnforceMode(Enforcing); err != nil {
+		t.Fatalf("enforcing selinux failed: %v", err)
 	}
+	if err := SetEnforceMode(Permissive); err != nil {
+		t.Fatalf("setting selinux mode to permissive failed: %v", err)
+	}
+	SetEnforceMode(mode)
+
+	pid := os.Getpid()
+	t.Logf("PID:%d MCS:%s\n", pid, intToMcs(pid, 1023))
+	err = SetFSCreateLabel("unconfined_u:unconfined_r:unconfined_t:s0")
+	if err == nil {
+		t.Log(FSCreateLabel())
+	} else {
+		t.Log("SetFSCreateLabel failed", err)
+		t.Fatal(err)
+	}
+	err = SetFSCreateLabel("")
+	if err == nil {
+		t.Log(FSCreateLabel())
+	} else {
+		t.Log("SetFSCreateLabel failed", err)
+		t.Fatal(err)
+	}
+	t.Log(PidLabel(1))
 }
 
 func TestCanonicalizeContext(t *testing.T) {
-	if GetEnabled() {
-		con := "system_u:object_r:bin_t:s0:c1,c2,c3"
-		checkcon := "system_u:object_r:bin_t:s0:c1.c3"
-		newcon, err := CanonicalizeContext(con)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if newcon != checkcon {
-			t.Fatalf("CanonicalizeContext(%s) returned %s expected %s", con, newcon, checkcon)
-		}
-		con = "system_u:object_r:bin_t:s0:c5,c2"
-		checkcon = "system_u:object_r:bin_t:s0:c2,c5"
-		newcon, err = CanonicalizeContext(con)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if newcon != checkcon {
-			t.Fatalf("CanonicalizeContext(%s) returned %s expected %s", con, newcon, checkcon)
-		}
+	if !GetEnabled() {
+		t.Skip()
+	}
+
+	con := "system_u:object_r:bin_t:s0:c1,c2,c3"
+	checkcon := "system_u:object_r:bin_t:s0:c1.c3"
+	newcon, err := CanonicalizeContext(con)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newcon != checkcon {
+		t.Fatalf("CanonicalizeContext(%s) returned %s expected %s", con, newcon, checkcon)
+	}
+	con = "system_u:object_r:bin_t:s0:c5,c2"
+	checkcon = "system_u:object_r:bin_t:s0:c2,c5"
+	newcon, err = CanonicalizeContext(con)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newcon != checkcon {
+		t.Fatalf("CanonicalizeContext(%s) returned %s expected %s", con, newcon, checkcon)
 	}
 }
