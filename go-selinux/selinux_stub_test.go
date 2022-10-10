0ee8872795cd49cc2e77a85f9d95cf6202228f5c
Sebastiaan van Stijn <github@gone.nl>
2022-10-10 23:39:56 +0200

    go-selinux: improve stub tests coverage
    - add missing asserts
    - use t.Error() instead of t.Fatal() to not fail early
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- go-selinux/selinux_stub_test.go
+++ go-selinux/selinux_stub_test.go
@@ -7,93 +7,113 @@ import (
 	"testing"
 )
 
-func TestSELinux(t *testing.T) {
+const testLabel = "foobar"
+
+func TestSELinuxStubs(t *testing.T) {
 	if GetEnabled() {
-		t.Fatal("SELinux enabled on non-linux.")
+		t.Error("SELinux enabled on non-linux.")
 	}
 
 	if _, err := FileLabel("/etc"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
-	if err := SetFileLabel("/etc", "foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetFileLabel("/etc", testLabel); err != nil {
+		t.Error(err)
 	}
 
-	if err := SetFSCreateLabel("foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetFSCreateLabel(testLabel); err != nil {
+		t.Error(err)
 	}
 
 	if _, err := FSCreateLabel(); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if _, err := CurrentLabel(); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	if _, err := PidLabel(0); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	ClearLabels()
 
-	ReserveLabel("foobar")
-	ReleaseLabel("foobar")
-	_, _ = DupSecOpt("foobar")
-	DisableSecOpt()
+	ReserveLabel(testLabel)
+	ReleaseLabel(testLabel)
+	if _, err := DupSecOpt(testLabel); err != nil {
+		t.Error(err)
+	}
+	if v := DisableSecOpt(); len(v) != 1 || v[0] != "disable" {
+		t.Errorf(`expected "disabled", got %v`, v)
+	}
 	SetDisabled()
 	if enabled := GetEnabled(); enabled {
-		t.Fatal("Should not be enabled")
+		t.Error("Should not be enabled")
 	}
-	if err := SetExecLabel("foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetExecLabel(testLabel); err != nil {
+		t.Error(err)
 	}
-	if err := SetTaskLabel("foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetTaskLabel(testLabel); err != nil {
+		t.Error(err)
 	}
 	if _, err := ExecLabel(); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	if _, err := CanonicalizeContext("foobar"); err != nil {
-		t.Fatal(err)
+	if _, err := CanonicalizeContext(testLabel); err != nil {
+		t.Error(err)
 	}
-	if _, err := ComputeCreateContext("foo", "bar", "foobar"); err != nil {
-		t.Fatal(err)
+	if _, err := ComputeCreateContext("foo", "bar", testLabel); err != nil {
+		t.Error(err)
 	}
-	if err := SetSocketLabel("foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetSocketLabel(testLabel); err != nil {
+		t.Error(err)
 	}
-	if _, err := ClassIndex("foobar"); err != nil {
-		t.Fatal(err)
+	if _, err := ClassIndex(testLabel); err != nil {
+		t.Error(err)
 	}
 	if _, err := SocketLabel(); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if _, err := PeerLabel(0); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	if err := SetKeyLabel("foobar"); err != nil {
-		t.Fatal(err)
+	if err := SetKeyLabel(testLabel); err != nil {
+		t.Error(err)
 	}
 	if _, err := KeyLabel(); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	con, err := NewContext("foobar")
+	if err := SetExecLabel(testLabel); err != nil {
+		t.Error(err)
+	}
+	if _, err := ExecLabel(); err != nil {
+		t.Error(err)
+	}
+	con, err := NewContext(testLabel)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	con.Get()
-	if err := SetEnforceMode(1); err != nil {
-		t.Fatal(err)
+	if err = SetEnforceMode(1); err != nil {
+		t.Error(err)
 	}
-	DefaultEnforceMode()
-	EnforceMode()
-	ROFileLabel()
-	ContainerLabels()
-	if err := SecurityCheckContext("foobar"); err != nil {
-		t.Fatal(err)
+	if v := DefaultEnforceMode(); v != Disabled {
+		t.Errorf("expected %d, got %d", Disabled, v)
 	}
-	if _, err := CopyLevel("foo", "bar"); err != nil {
-		t.Fatal(err)
+	if v := EnforceMode(); v != Disabled {
+		t.Errorf("expected %d, got %d", Disabled, v)
+	}
+	if v := ROFileLabel(); v != "" {
+		t.Errorf(`expected "", got %q`, v)
+	}
+	if processLbl, fileLbl := ContainerLabels(); processLbl != "" || fileLbl != "" {
+		t.Errorf(`expected fileLbl="", fileLbl="" got processLbl=%q, fileLbl=%q`, processLbl, fileLbl)
+	}
+	if err = SecurityCheckContext(testLabel); err != nil {
+		t.Error(err)
+	}
+	if _, err = CopyLevel("foo", "bar"); err != nil {
+		t.Error(err)
 	}
 }
