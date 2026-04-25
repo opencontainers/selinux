93c00a10efceaac7adc0d08a3bab54ec27bc4f7b
Kir Kolyshkin <kolyshkin@gmail.com>
2026-04-24 18:07:54 -0700

    Deprecate ROFileLabel
    This function has no users, and its implementation is questionable: one
    has to call ContainerLabels first, or they will get an empty label).
    
    This will either need to be removed, or changed to (say):
    
    	if l := label("ro_file"); l != "" {
    		return l
    	}
    	return label("file")
    
    (optionally wrapped into sync.OnceValue).
    
    I vouch for removal (as it has no users) via deprecation.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- go-selinux/label/label_stub_test.go
+++ go-selinux/label/label_stub_test.go
@@ -4,8 +4,6 @@ package label
 
 import (
 	"testing"
-
-	"github.com/opencontainers/selinux/go-selinux"
 )
 
 const testLabel = "system_u:object_r:container_file_t:s0:c1,c2"
@@ -18,9 +16,6 @@ func TestInit(t *testing.T) {
 		t.Fatal(err)
 	}
 	testDisabled := []string{"disable"}
-	if selinux.ROFileLabel() != "" {
-		t.Error("selinux.ROFileLabel Failed")
-	}
 	plabel, mlabel, err := InitLabels(testDisabled)
 	if err != nil {
 		t.Log("InitLabels Disabled Failed")
